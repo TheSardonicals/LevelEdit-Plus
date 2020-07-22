a949c74e79e9993a2f224cfbcda8879b4c457bd7
isaboll1 <isaboll12@gmail.com>
2020-07-22 11:41:17 -0400

    main.cpp should use "int main()", as that is the standard entry point for all cpp programs

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "headers.h"
 
-int Start(int argc, char* args[]){
+int main(int argc, char* args[]){
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *win = NULL;
     SDL_Event event;
     SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, 0);
 
-    if (SDL_INIT(SDL_INIT_VIDEO) < 0){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0){
         printf("SDL_VIDEO not initialized");
     }else{
         while (running){
